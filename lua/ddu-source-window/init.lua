c4e522b34bc6249fb73825ebda7f19f08b87707e
kamecha <masakikameyama81@gmail.com>
2023-08-15 21:42:42 +0900

    nvim_list_tabpagesを用いたtabpageリストの配列取得

--- lua/ddu-source-window/init.lua
+++ lua/ddu-source-window/init.lua
@@ -1,5 +1,17 @@
 local M = {}
 
+---get tabpages
+---@return number[]
+function M.get_tabpages()
+	local tabpages =  vim.api.nvim_list_tabpages()
+	local ret = {}
+	for _, tabpage in ipairs(tabpages) do
+		print(tabpage)
+		table.insert(ret, tabpage)
+	end
+	return ret
+end
+
 ---get tab's name
 ---@param tabid number
 ---@return string
