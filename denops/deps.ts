583782ea59a07b47bc90f354c22fe1796a3cabe4
kamecha <masakikameyama81@gmail.com>
2024-01-29 00:30:39 +0900

    update deps

--- denops/deps.ts
+++ denops/deps.ts
@@ -1,12 +1,12 @@
-export { fn } from "https://deno.land/x/ddu_vim@v2.5.0/deps.ts";
-export type { Denops } from "https://deno.land/x/ddu_vim@v2.5.0/deps.ts";
+export { fn } from "https://deno.land/x/ddu_vim@v3.10.1/deps.ts";
+export type { Denops } from "https://deno.land/x/ddu_vim@v3.10.1/deps.ts";
 export {
   ActionFlags,
   BaseColumn,
   BaseKind,
   BaseSource,
-} from "https://deno.land/x/ddu_vim@v2.5.0/types.ts";
-export type { DduOptions } from "https://deno.land/x/ddu_vim@v2.5.0/types.ts";
+} from "https://deno.land/x/ddu_vim@v3.10.1/types.ts";
+export type { DduOptions } from "https://deno.land/x/ddu_vim@v3.10.1/types.ts";
 export type {
   Actions,
   Context,
@@ -14,16 +14,16 @@ export type {
   Item,
   PreviewContext,
   Previewer,
-} from "https://deno.land/x/ddu_vim@v2.5.0/types.ts";
+} from "https://deno.land/x/ddu_vim@v3.10.1/types.ts";
 export type {
   GetLengthArguments,
   GetTextArguments,
   GetTextResult,
-} from "https://deno.land/x/ddu_vim@v2.5.0/base/column.ts";
+} from "https://deno.land/x/ddu_vim@v3.10.1/base/column.ts";
 export {
   ensureArray,
   ensureNumber,
   ensureObject,
   ensureString,
 } from "https://deno.land/x/unknownutil@v2.1.0/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.177.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.213.0/testing/asserts.ts";
